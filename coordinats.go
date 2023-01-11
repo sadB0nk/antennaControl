dbf360c8b6c739463466e3701f0539fc8474f4f2
sadB0nk <cherepanovsi94@gmail.com>
2023-01-11 15:48:58 +0300

    FirstCommit

---
+++ coordinats.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Coordinats struct {
+	Az float64
+	El float64
+}
+
+func (c Coordinats) String() string {
+	return fmt.Sprintf("Az:%g	El:%g", c.Az, c.El)
+}
+func (c Coordinats) GetPosition() string {
+	return fmt.Sprintf("%g\n%g\n", c.Az, c.El) // Az\nEl\n
+}
+func (c Coordinats) GetPositionMassive() []float64 {
+	return []float64{c.Az, c.El}
+}
+
+/*
+func (c Coordinats) SetCoordinats(b []byte) {
+	c.Az = b[0]
+
+}
+*/
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func readBytes(reader *bufio.Reader) string {
+	for {
+		bytik, err := reader.ReadByte()
+		if err != nil {
+			break
+		}
+		data = append(data, bytik)
+		if bytik == 112 {
+			break
+		}
+	}
+	return "tmp"
+}
+func main() {
+	const (
+		getPos   byte = 112
+		setPos   byte = 80
+		shutdown byte = 83
+		buffSize      = 256
+	)
+	log.Printf("Starting tcp server")
+	listener, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	var coord Coordinats
+	coord.Az = 17
+	coord.El = 12
+	fmt.Println(coord.GetPositionMassive())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Connect to ip = %v", conn.RemoteAddr())
+		buff := bufio.NewReader(conn)
+		for {
+			fmt.Println(coord.GetPositionMassive())
+			data := readBytes(buff)
+
+			fmt.Println(data)
+			if data[0] == getPos {
+				_, err := io.WriteString(conn, coord.GetPosition())
+				if err != nil {
+					return
+				}
+			}
+			log.Printf("Recieved data: %s", data)
+
+			log.Printf("Recieved data: %v", data)
+			if data[0] == 83 {
+				fmt.Println("Было разорвано соединение с Gpredict")
+				conn.Close()
+				break
+			}
+
+		}
+	}
+}
